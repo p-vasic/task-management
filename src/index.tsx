8a61e464d0a822917012694c9aa0e1c508c2f754
p-vasic <98508137+p-vasic@users.noreply.github.com>
2022-05-23 11:34:36 -0700

    Revert "Feat/homepage"
    This reverts commit 19b2a29d997584c222a40d07ff8503f35984b968.

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/system';
-import { createTheme } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={createTheme()}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
