dc0c34e05fcb8d7330be83793f78ad8fe791083d
p-vasic <vasicdeveloperbg@gmail.com>
2022-05-24 12:13:27 +0200

    feat: Update style of task panel

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -13,7 +13,7 @@ const Tasks: FC = () => {
       </Typography>
 
       <Box mt={3} bgcolor="#75a7d9" height="90vh" borderRadius={5} p={3}>
-        <Grid container>
+        <Grid container spacing={1}>
           {tasks.length === 0 ? (
             <Typography variant="subtitle1">
               You have nothing to do. Go get some sleep.
